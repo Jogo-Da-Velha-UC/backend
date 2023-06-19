f32af1d917cb07660e84bbe352109c716a4313eb
MatheusBonnet <matheusbonnet2019@gmail.com>
2023-06-19 12:26:00 -0300

    Implementado teste de integracao

--- src/test/java/com/br/uni/edu/jogoDaVelha/resourceTest/GameIntegrationTest.java
+++ src/test/java/com/br/uni/edu/jogoDaVelha/resourceTest/GameIntegrationTest.java
@@ -1,43 +1,54 @@
-//package com.br.uni.edu.jogoDaVelha.resourceTest;
-//
-//import com.br.uni.edu.jogoDaVelha.repositories.GameStructRepository;
-//import com.br.uni.edu.jogoDaVelha.repositories.MatchRepository;
-//import com.br.uni.edu.jogoDaVelha.repositories.StatusMatchRepository;
-//import com.br.uni.edu.jogoDaVelha.requests.CreateGameRequest;
-//import com.br.uni.edu.jogoDaVelha.service.PlayerService;
-//import com.br.uni.edu.jogoDaVelha.service.impl.MatchServiceImpl;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//
-//@SpringBootTest
-//public class GameIntegrationTest {
-//
-//    @InjectMocks
-//    MatchServiceImpl matchService;
-//    @MockBean
-//    private  PlayerService playerService;
-//    @MockBean
-//    private  MatchRepository matchRepository;
-//    @MockBean
-//    private  GameStructRepository gameStructRepository;
-//    @MockBean
-//    private  StatusMatchRepository statusMatchRepository;
-//
-//    @BeforeEach
-//    public void setup(){
-//        matchService = new MatchServiceImpl(playerService,matchRepository,gameStructRepository,statusMatchRepository, matchMapper);
-//    }
-//
-//    @Test
-//    void createGame() throws Exception {
-//
-//        CreateGameRequest createGameRequest = new CreateGameRequest();
-//        createGameRequest.setPlayer("nicolas");
-//        matchService.createMatch(createGameRequest);
-//    }
-//
-//}
+package com.br.uni.edu.jogoDaVelha.resourceTest;
+
+import com.br.uni.edu.jogoDaVelha.requests.CreateGameRequest;
+import com.br.uni.edu.jogoDaVelha.requests.MoveRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GameIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void createGame() throws Exception {
+
+        CreateGameRequest createGameRequest = new CreateGameRequest();
+        createGameRequest.setPlayerOne("nicolas");
+        createGameRequest.setPlayerTwo("joao");
+
+        mockMvc.perform(post("/api/v1/game/create-game")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(createGameRequest)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void makeMove() throws Exception {
+
+        MoveRequest moveRequest = new MoveRequest();
+        moveRequest.setRol("0");
+        moveRequest.setCol("1");
+        moveRequest.setMatchId(3L);
+        moveRequest.setPlayer("joao");
+        mockMvc.perform(post("/api/v1/game/make-move")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(moveRequest)))
+                .andExpect(status().isOk());
+
+    }
+
+}
